493a3cd4ecc29e8fe6bb0080517b96348371a72c
archana <archanarackenchath@gmail.com>
2026-05-06 22:05:42 +0530

    fix production api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,10 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:62481/api',
+//   appName: 'HotelOS'
+// };
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:62481/api',
+  production: true,
+  apiUrl: 'https://hotelos-api-30uy.onrender.com/api',
   appName: 'HotelOS'
 };
